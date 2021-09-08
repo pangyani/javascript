276428123d690993bc01228feb40edf441d4bc7e
pangyani <pangyanishiva@gmail.com>
2021-09-08 09:22:09 +0545

    second commit

---
+++ imageslider.js
@@ -0,0 +1,19 @@
+let img_array=['a.jpg','b.jpg','c.jpg','d.jpg','e.jpg']
+let array_length=img_array.length
+let i=0
+let set=setInterval(slider,5000)
+//slider ->user define function
+//5000 -> 5 seconds
+
+function slider(){
+    i++
+    i=i%array_length
+    // i=0%5=0
+    //1%5=1
+    //2%5=2
+    //3%5=3
+    //4%5=4
+    //5%5=0
+    document.getElementById('images/')+img_array[i]
+
+}
